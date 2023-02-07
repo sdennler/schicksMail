67b311defe05fafb2a4f1e40fec849cead1aef5f
Samuel Dennler <834472+sdennler@users.noreply.github.com>
2023-02-07 09:30:25 +0100

    Move js to public and link it

---
+++ public/schicksMail.js
@@ -0,0 +1,33 @@
+$(function() {
+    var form = $('#schicksMail');
+    var mesg = $('#schicksMailResponse');
+
+    $(form).submit(function(event) {
+        $(mesg).removeClass('error');
+        $(mesg).removeClass('success');
+        $(mesg).html('Sending your message ✉ ✈ 🙌');
+        event.preventDefault();
+
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: $(form).serialize()
+        })
+
+        .done(function(response) {
+            $(mesg).addClass('success');
+            $(mesg).html(response.replace(/\n/g, '<br>'));
+            $(form)[0].reset();
+        })
+
+        .fail(function(data) {
+            $(mesg).addClass('error');
+            if (data.responseText !== '') {
+                $(mesg).html(data.responseText.replace(/\n/g, '<br>'));
+            } else {
+                $(mesg).html('Oh no! Some thing went wrong! 😕');
+            }
+        });
+
+    });
+});
